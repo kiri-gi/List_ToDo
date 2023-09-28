9dac97f57c2606a4b0580b0e3425262f7f25ed95
VINICIUS DE OLIVEIRA SOUZA <vinicius.souza@dominio.supermed>
2023-09-28 14:50:13 -0300

    new line

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs';
 const API = "http://localhost:5000";
 
 function App() {
-  const [title, setTitle] = useState("")
-  const [time, setTime] = useState("")
-  const [todos, setTodos] = useState([])
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
   //Load todos on page load
